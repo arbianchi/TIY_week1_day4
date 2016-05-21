c43005536da170870d806f5d9bdf747b2da52380
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-05-21 15:18:30 -0400

    everything is working

--- memory.rb
+++ memory.rb
@@ -14,6 +14,7 @@ def generate_answer_key start_board
 end
 
 def print_board board
+
   puts
   print board[0..3].join
   puts
@@ -23,18 +24,7 @@ def print_board board
   puts
 end
 
-def choose_letter choice
-
-  if choice == "choice1"
-    puts "Choose a letter: "
-
-    choice1 = gets.chomp.upcase
-  else
-    puts "Choose another letter: "
-
-    choice2 = gets.chomp.upcase
-  end
-end
+print_board correct_answers_board
 
 answer_key = generate_answer_key start_board
 
@@ -42,27 +32,35 @@ puts answer_key # REMOVE
 
 until matches == 6 || attempts == 0
 
-  puts "Choose a letter: "
+  temp_board = correct_answers_board.clone
 
+  puts "Choose a letter: "
   choice1 = gets.chomp.upcase
 
   puts "Choose another letter: "
-
   choice2 = gets.chomp.upcase
 
-  if answer_key[choice1] != answer_key[choice2]
+  if choice1.to_i != 0 || choice2.to_i != 0
+
+    puts "Enter two valid LETTERS A-L please."
+
+  elsif answer_key[choice1] != answer_key[choice2]
+
     temp_board[start_board.index(choice1)] = answer_key[choice1]
     temp_board[start_board.index(choice2)] = answer_key[choice2]
     print_board temp_board
     puts
     puts "No match! Try again."
     attempts -= 1
-  else
+
+  else answer_key[choice1] == answer_key[choice2]
+
     puts "You found a match!"
     correct_answers_board[start_board.index(choice1)] = answer_key[choice1]
     correct_answers_board[start_board.index(choice2)] = answer_key[choice2]
     matches += 1
     attempts -= 1
+
   end
   print_board correct_answers_board
 
